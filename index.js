0382c879e1edb86fea9642953b14f564fc6aeb69
dannyjanneman <43755839+dannyjanneman@users.noreply.github.com>
2022-09-26 21:28:28 +0200

    Added discord bot

---
+++ index.js
@@ -0,0 +1,37 @@
+const {Client, RichEmbed} = require('discord.js');
+const bot = new Client();
+
+const token = 'MTAwODg3Mzc1ODU1NzIyMDk0Ng.GGu-2d.OfvGfnuRVourP9k5oP7jPiSZoVYHA79NypsMlk';
+
+const PREFIX = '!';
+
+bot.on('ready' , () =>{
+    console.log('The bot is online');
+});
+
+bot.on('message', message=>{
+
+    let args = message.content.substring(PREFIX.length).split(" ")
+
+    switch (args[0]) {
+        case 'ping':
+            message.channel.send('pong!')
+            break;
+        case 'info':
+            if (args[1] === 'version') {
+                message.channel.send('version 1')
+            }
+            else{
+                message.channel.send('invalid')
+            }
+            break;
+        case 'clear':
+            if(!args[1]) return message.reply('Error please define second argument')
+            message.channel.bulkDelete(args[1]);
+            break;
+    }
+
+
+})
+
+bot.login(token);
